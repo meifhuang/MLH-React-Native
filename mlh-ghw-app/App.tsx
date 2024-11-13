b5ad4f5842f1e00589e8a07cc437e015309165af
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2024-11-13 16:00:50 -0500

    Add some styling and basic text to start

--- mlh-ghw-app/App.tsx
+++ mlh-ghw-app/App.tsx
@@ -1,11 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Alert, Linking} from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
+
+  const handleSignUp = () => {
+    Alert.alert("Register for GHW", "Are you interested to participate in MLH?",
+      [{text: "Nope",
+        style: "destructive"
+      }, 
+      {text: "Yes!",
+        style: "default",
+        onPress: () => Linking.openURL("https://ghw.mlh.io/schedule")
+      }]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>MY REACT NATIVE APP</Text>
+      <Text style={styles.headlineText}> MLH REACT NATIVE APP </Text>
+      <Text> GLOBAL HACK WEEK </Text>
+      <Text style={styles.bodyText}> Everyday we have a variety of live sessions for you to tune into and learn more about. Click below to see each day's schedule.</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity style={styles.signUpButton} onPress={handleSignUp}> 
+        <Text style={styles.signUpButtonText}> Sign Up</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +31,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
-    backgroundColor: "#fff",
+    backgroundColor: theme.colors.white,
     flex: 1,
     justifyContent: "center",
   },
+  bodyText: {
+    fontSize: 16,
+    fontFamily: "System",
+    color: "black",
+    width: "80%", 
+    marginVertical: 10, 
+  }, 
+  headlineText: {
+    ...theme.textVariants.headLine,
+    fontWeight: "bold"
+  },
+  signUpButton: {
+    color: theme.colors.white,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 6,
+    padding: 16
+  },
+  signUpButtonText: {
+    color: theme.colors.white 
+  }
+  
 });
---
+++ mlh-ghw-app/theme.ts
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    white: "#ffffff",
+    black: "#000000", 
+    primary: "#5a43f4"
+  },
+  textVariants: {
+    headLine: {
+        fontSize: 32,
+        fontFamily: "System",
+        color: "black",   
+    }
+  }
+};
