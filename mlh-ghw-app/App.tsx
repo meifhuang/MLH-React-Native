46f007774a30ccb26c1ad88f203ab547ba170524
Mei Huang <100555326+meifhuang@users.noreply.github.com>
2024-11-13 15:12:39 -0500

    Set up react native app with eslint and prettier

---
+++ mlh-ghw-app/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>MY REACT NATIVE APP</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    flex: 1,
+    justifyContent: "center",
+  },
+});
